647d8dc5f499a9f6b57c144f1abc3a3bdb38a660
ajfred20 <ajfred2008@gmail.com>
2024-12-07 10:53:21 +0100

    firstbatch of code and design sent

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,25 @@
-import { Footer, Navbar } from "@/components";
-import { SITE_CONFIG } from "@/config";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
-import { ClerkProvider } from '@clerk/nextjs';
-import { dark } from '@clerk/themes';
-import { Inter } from "next/font/google";
 
 const font = Inter({ subsets: ["latin"] });
 
-export const metadata = SITE_CONFIG;
-
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-            <body
-                className={cn(
-                    "min-h-screen bg-background text-foreground antialiased max-w-full overflow-x-hidden",
-                    font.className
-                )}
-            >
-                <ClerkProvider appearance={{ baseTheme: dark }}>
-                    {children}
-                </ClerkProvider>
-            </body>
-        </html>
-    );
-};
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background text-foreground antialiased max-w-full overflow-x-hidden",
+          font.className
+        )}
+      >
+        <ClerkProvider>{children}</ClerkProvider>
+      </body>
+    </html>
+  );
+}
